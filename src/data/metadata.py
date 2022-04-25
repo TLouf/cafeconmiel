c5df55a8489235ac6daab8686391315c7d454712
TLouf <loufthomas@gmail.com>
2022-04-25 11:26:26 +0200

    Fix normalize

--- src/data/metadata.py
+++ src/data/metadata.py
@@ -2,7 +2,16 @@
 
 def normalize(df):
     if 'year' in df.columns:
-        df['year'] = df['year'].str.extract(r'([0-9]{4})').astype(float)
+        has_year = df['year'].notnull()
+        df.loc[has_year, 'year'] = (
+            df.loc[has_year, 'year'].str.extract(r'([0-9]{4})', expand=False)
+            .astype(float)
+        )
+        if 'date' in df.columns:
+            df.loc[~has_year, 'year'] = (
+                df.loc[~has_year, 'date'].str.extract(r'([0-9]{4})', expand=False)
+                .astype(float)
+            )
         # year_isnull = (~df['year'].str.isdigit()) | (df['year'] == '0')
         # # approx year by mid century, not possible for codea because when year is null, century too.
         # # df.loc[year_isnull, 'date'] = df.loc[year_isnull, 'century'].apply(lambda c: roman.fromRoman(c)*100+50)
@@ -15,7 +24,11 @@ def normalize(df):
     place_cols = ['locality', 'region']
     if 'place' in df.columns:
         has_place = df['place'].notnull()
-        df.loc[has_place, place_cols] = df.loc[has_place, 'place'].str.split(', ', expand=True)
+        df.loc[has_place, place_cols] = (
+            df.loc[has_place, 'place']
+             .str.split(', ', expand=True)
+             .rename(columns={i: place_cols[i] for i in range(len(place_cols))})
+        )
     for col in place_cols:
         df[col] = df[col].str.strip(to_strip=' ?[]').str.title()
         df.loc[df[col].str.len() == 1, col] = None
