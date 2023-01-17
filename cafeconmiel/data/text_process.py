0107bfec96bd302853741780df15f48cac3060aa
TLouf <loufthomas@gmail.com>
2023-01-17 14:12:39 +0100

    Add seseo NB

--- cafeconmiel/data/text_process.py
+++ cafeconmiel/data/text_process.py
@@ -30,32 +30,36 @@ def remove_from_list(text, pattern, replace_list):
 
 
 def seseo_corr(w, dictionary):
-    list_s = ['s', 'z', 'c']
-    matches = list(re.finditer(r'(s)|(z)|(ci)|(ce)', w))
+    possible_switches = {
+        'ss': {'always': ['s', 'z'], 'ie_next': ['c']},
+        's': {'always': ['z'], 'ie_next': ['c']},
+        'z': {'always': ['s'], 'ie_next': ['c']},
+        'c': {'always': ['s', 'z']},
+    }
+    # 'ss' needs to be first to avoid double 's' match
+    matches = list(re.finditer(r'(ss)|(s)|(z)|(c)(?:i|e)', w))
     nr_matches = len(matches)
     if nr_matches > 0 and w not in dictionary:
         list_switches = []
         for m in matches:
             grps = m.groups()
             idx_match_group = [i for i in range(len(grps)) if grps[i] is not None][0]
+            # .group() returns ci or ce, while in .groups() you only have c
+            s_char = grps[idx_match_group]
             start, end = m.span()
-            if end - start == 2: # ci or ce
+            if s_char == 'c': # ci or ce
                 next_char = w[end - 1]
             elif end < len(w):
                 next_char = w[end]
             else:
                 next_char = ''
 
-            if next_char == '': # end of word
-                # can only be matched if last statement above is true
-                switch_to_idc = [1 - idx_match_group] # 0 -> 1 or 1 -> 0
-            elif next_char in ('i', 'e'): 
+            switch_to_char = possible_switches[s_char]['always']
+            if next_char in ('i', 'e'): 
                 # can be matched when one of first two statements above is true
-                switch_to_idc = [i for i in range(3) if i != idx_match_group]
-            else: # followed by a, o, u or consonant
-                # can only be matched when second statement above is true
-                switch_to_idc = [1 - idx_match_group]
-            list_switches.append(switch_to_idc)
+                switch_to_char.extend(possible_switches[s_char].get('ie_next', []))
+
+            list_switches.append(switch_to_char)
 
         for nr_subs in range(1, nr_matches + 1):
             # depending on number of substitutions (subs) you do at once, you'll have
@@ -67,20 +71,22 @@ def seseo_corr(w, dictionary):
                 # For one subset of matches, there can be more different subs
                 # to perform than the size of the subset, as for some matches
                 # ((s|z|c)(i|e)) there are 2 different subs that can be made.
-                possible_s_indices = list(
+                possible_s = list(
                     itertools.product(*[list_switches[m_idx] for m_idx in m_idc])
                 )
 
                 # This time, one mod_w per iter of next loop:
-                for s_idc in possible_s_indices:
+                for s_chars in possible_s:
                     # Reset to original word and perform all `nr_subs` subs
                     mod_w = w
                     seseo_pos = []
-                    for m_idx, s_idx in zip(m_idc, s_idc):
+                    for m_idx, s in zip(m_idc, s_chars):
                         m = matches[m_idx]
                         seseo_pos.append(str(m.start()))
-                        s = list_s[s_idx]
-                        mod_w = mod_w[:m.start()] + s + mod_w[m.start() + 1:]
+                        nr_chars_to_sub = 1
+                        if m.group() == 'ss':
+                            nr_chars_to_sub += 1
+                        mod_w = mod_w[:m.start()] + s + mod_w[m.start() + nr_chars_to_sub:]
 
                     if mod_w in dictionary:
                         return mod_w, ', '.join(seseo_pos)
