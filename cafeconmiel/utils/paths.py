908eba4228cc9928f2a04f4aab549345701198e1
TLouf <loufthomas@gmail.com>
2023-01-17 14:11:28 +0100

    Cleanup `ProjectPaths`

--- cafeconmiel/utils/paths.py
+++ cafeconmiel/utils/paths.py
@@ -54,30 +54,12 @@ class ProjectPaths:
     this class allows us to define these only once, ensuring consistency.
     '''
     proj: Path = Path(os.environ['PROJ_DIR'])
-    # source_fname_fmt: str = '{kind}_{from}_{to}_{cc}.json.gz'
-    # counts_fname_fmt: str = ('{kind}_lang={lc}_cc={cc}_years={year_from}'
-    #                          '-{year_to}_cell_size={cell_size}.parquet')
-    # monthly_counts_fname_fmt: str = ('{kind}_lang={lc}_cc={cc}_year={year}'
-    #                                  '_month={month}_cell_size={cell_size}.parquet')
-    # cluster_fig_fname_fmt: str = (
-    #     'clusters_method={method_repr}{kwargs_str}_word_vec_var={word_vec_var}'
-    #     '_decomposition={decomposition}.pdf')
-    # decomp_fig_fname_fmt: str = (
-    #     'decomp_word_vec_var={word_vec_var}_component{component}.pdf'
-    # )
-    # net_fname_fmt: str = (
-    #     'net_metric={metric}_transfo={transfo_str}_word_vec_var={word_vec_var}'
-    #     '_decomposition={decomposition}.dat')
-    # source_fmt: Path = field(init=False)
     proj_data: Path = field(init=False)
     ext_data: Path = field(init=False)
     raw_data: Path = field(init=False)
     interim_data: Path = field(init=False)
     processed_data: Path = field(init=False)
-    # counts_files_fmt: Path = field(init=False)
-    # shp_file_fmt: Path = field(init=False)
-    # figs: Path = field(init=False)
-    # cluster_fig_fmt: Path = field(init=False)
+    figs: Path = field(init=False)
 
     def __post_init__(self):
         self.proj_data = self.proj / 'data'
@@ -85,22 +67,5 @@ class ProjectPaths:
         self.raw_data = self.proj_data / 'raw'
         self.interim_data = self.proj_data / 'interim'
         self.processed_data = self.proj_data / 'processed'
-        # self.counts_files_fmt = self.raw_data / '{lc}' / '{cc}' / self.counts_fname_fmt
-        # self.monthly_counts_files_fmt = self.raw_data / '{lc}' / '{cc}' / self.monthly_counts_fname_fmt
-        # self.shp_file_fmt = self.ext_data / '{0}.shp' / '{0}.shp'
-        # self.figs = self.proj / 'reports' / 'figures'
-        # self.case_figs = self.figs / '{lc}' / '{cc}' / '{year_from}-{year_to}'
-        # self.cluster_fig_fmt = self.case_figs / self.cluster_fig_fname_fmt
-        # self.decomp_fig_fmt = self.case_figs / self.decomp_fig_fname_fmt
-        # self.net_fmt = self.processed_data / '{lc}' / '{cc}' / self.net_fname_fmt
-
-
-    # def partial_format(self, **kwargs):
-    #     self.counts_files_fmt = Path(partial_format(str(self.counts_files_fmt), **kwargs))
-    #     self.monthly_counts_files_fmt = Path(partial_format(
-    #         str(self.monthly_counts_files_fmt), **kwargs
-    #     ))
-    #     self.case_figs = Path(partial_format(str(self.case_figs), **kwargs))
-    #     self.cluster_fig_fmt = self.case_figs / self.cluster_fig_fname_fmt
-    #     self.decomp_fig_fmt = self.case_figs / self.decomp_fig_fname_fmt
-    #     self.net_fmt = Path(partial_format(str(self.net_fmt), **kwargs))
+        self.figs = self.proj / 'reports' / 'figures'
+        self.charact_words_freqs = self.figs / '{corpus_name}' / 'characteristic_words_freqs'
